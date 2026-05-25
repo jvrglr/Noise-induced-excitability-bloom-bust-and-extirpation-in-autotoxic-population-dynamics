c4252a8037b30ee0e5d03d4b38eabec5874bf123
Javier Aguilar Sánchez <jvrglrschz@gmail.com>
2026-05-25 10:55:53 +0200

    Update main_Draw_trajectories.f90

--- Scripts/main_Draw_trajectories.f90
+++ Scripts/main_Draw_trajectories.f90
@@ -14,7 +14,7 @@ program main_program
     ! D = 0.4d0
     r =   1.000000000000000      
     D = 0.14384498882876628    
-    call Draw_trajectory(until_t=100.0d0,init_t=0.0d0,dt=dt,init_x=0.01d0,init_y=0.0d0,points=int8(10000),name_traj = "data/traj_D_0d14_r_1.dat",until_extinction=.false.,print_theta_w=.true.)
+    call Draw_trajectory(until_t=100.0d0,init_t=0.0d0,dt=dt,init_x=0.01d0,init_y=0.0d0,points=int8(10000),name_traj = "filename.dat",until_extinction=.false.,print_theta_w=.true.)
     
   101 end program main_program
   
